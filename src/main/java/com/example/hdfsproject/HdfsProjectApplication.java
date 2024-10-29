3160f9c3ca880081b3c5d6bce00750f4697a0bc2
utkualcan <aalcan.utku@gmail.com>
2024-10-29 19:28:57 +0300

    Initial commit

---
+++ src/main/java/com/example/hdfsproject/HdfsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.hdfsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdfsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HdfsProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hdfsproject/HdfsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hdfsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdfsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
